65dc96e418248384cd811cd575be567c76f3e8eb
Diêgo Ernandes <diegobass405@gmail.com>
2025-04-05 17:09:43 -0300

    listas

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,20 @@
+/// <reference types = "cypress"/>
+
+describe('Funcionalidade: produtos', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block ')
+            //.first() Pega o primeiro item da lista
+            //.last() Pega o ultimo item da lista
+            //.eq(2) Pega o item dois do índice que começa em zero
+            .contains('Ariel Roll Sleeve Sweatshirt') //Pega o item pelo nome
+            .click()
+
+            cy.get('#tab-title-description > a').should('exist')
+        
+    });
+    
+});
