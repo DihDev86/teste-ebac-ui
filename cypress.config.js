12a959493a7d2c30ac521807d1364173f2e96a1c
Diêgo Ernandes <diegobass405@gmail.com>
2025-05-15 09:05:09 -0300

    Modos de execução e relátorios

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "a3jaa9",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://lojaebac.ebaconline.art.br/'
+    baseUrl: 'http://lojaebac.ebaconline.art.br/',
     //Alteração global. Essa baseUrl serve para ser usada nos cy.visit 
     //Tiramos a baseUrl e dexamos somente a funcionalidade, ex.: cy.visit('minha-conta')
+    
+    video: true,
+    //Serve para gerar vídeos de toda execução do teste em "Headless".
   },
 });
